89956fa17442a0eff87285b04a034bc8fd1578eb
linyeli <linyl@zjrealtech.com>
2017-08-25 17:52:52 +0800

    demo build

--- src/main/java/com/weather/WeatherApplication.java
+++ src/main/java/com/weather/WeatherApplication.java
@@ -3,10 +3,13 @@ package com.weather;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Created by dell on 2017/8/25.
+ */
 @SpringBootApplication
 public class WeatherApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WeatherApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherApplication.class, args);
+    }
 }
